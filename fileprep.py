41df46b5254f334c246e1ebc8570f3c0b728883f
Eric Boxer <eboxhead@gmail.com>
2016-07-02 10:34:07 -0400

    Added the ability to use a sub directory

--- fileprep.py
+++ fileprep.py
@@ -6,10 +6,12 @@ import os, re, argparse, csv
 
 csvFileName = "rename.csv"
 
+
 # ===============
 # Commandline Arguments
 # ===============
 parser = argparse.ArgumentParser(description="Rename files to have a leading number and a space")
+parser.add_argument('-f', help = "The folder with files to rename.", default = "")
 parser.add_argument('-e', help="match particular file extension. Defaults to using .wav", default = "wav")
 parser.add_argument('-s', help="What number to start from. Default is 1.", default = 1, type = int)
 parser.add_argument('-i', help="The increment value. Deafault is 1.", default = 1, type = int)
@@ -17,26 +19,40 @@ parser.add_argument('-l', help="Location this line is used. Will not include if 
 parser.add_argument('-p', help = "How many 0's to pad the leading number with. Default is 3.", default = 3, type = int)
 parser.add_argument('-c', help = "By default outputs to the command line only. Set true commit renaming to the directory.", action = "store_true", default = False)
 parser.add_argument('-u', help = "In case of dumb.", action = "store_true", default = False)
+parser.add_argument('-a', help = "Passthru.", action = "store_true", default = False)
+
 
 args = vars(parser.parse_args())
+
+folderToUse = os.path.join(os.path.curdir,args['f'])
+
+renameCSVPath = os.path.join(folderToUse,csvFileName)
+
+print folderToUse
+
+if args['a']:
+    exit()
 
 # ===============
 # Undo in case of Dumb
 # ===============
 
 if args['u']:
-    if os.path.exists(csvFileName):
+    if os.path.exists(renameCSVPath):
 
-        f = open(csvFileName, mode="rb")
+        f = open(renameCSVPath, mode="rb")
 
         csvF = csv.reader(f, delimiter = ",")
 
         for row in csvF:
-            os.rename(row[0], row[1])
+            os.rename(os.path.join(folderToUse, row[0]),os.path.join(folderToUse, row[1]))
+            print("%s renamed back to %s." % (row[0], row[1]))
         f.close()
-        os.remove(csvFileName)
+        print(".::All Files Renamed::.")
+        os.remove(renameCSVPath)
+        print(".::Undo File Removed For Your Safety::.")
     else:
-        print("Renaming file doesnt exist. You're SOL.")
+        print(".::Renaming file doesnt exist. You're SOL::.")
     exit()
 
 # ===============
@@ -55,13 +71,13 @@ else:
 # ===============
 # Rename the Files
 # ===============
-for directoryFile in os.listdir('./'):                                                     # Uses the directory the script is in. TODO: update to allow to choose directory.  
+for directoryFile in os.listdir(folderToUse):                                                     # Uses the directory the script is in. TODO: update to allow to choose directory.  
     if directoryFile.endswith(args['e']):
         oldName = directoryFile
         newName = str(fleCount).zfill(args['p']) + locationString +(re.sub('^[0-9_ ]*', '', directoryFile))                                   # Remove leading numbers, spaces, and underscores and then format the name.
-        print("%s renamed to %s" % (oldName, newName))
+        print("%s is now %s" % (oldName, newName))
         if args['c']:                                                                  # Make the changes permanant. 
-            os.rename(oldName, newName)
+            os.rename(os.path.join(folderToUse,oldName), os.path.join(folderToUse, newName))
         dataToCSV.append("%s,%s" % (newName, oldName))
         fleCount = fleCount + args['i']
 
@@ -71,10 +87,10 @@ print(".::Renaming Completed::.")
 # Write a Rename File (rename.csv)
 # ===============
 
-if os.path.exists(csvFileName):
-    os.remove(csvFileName)
+if os.path.exists(renameCSVPath):
+    os.remove(renameCSVPath)
 
-f = open(csvFileName, "wb")
+f = open(renameCSVPath, "wb")
 w = csv.writer(f, delimiter = ',')
 w.writerows([x.split(',') for x in dataToCSV])
 f.close()
